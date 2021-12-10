e9b81ff8507958a707ede7de73f11432c169c98e
salzanopm <salzanopm@email.com>
2021-12-10 15:38:01 +0100

    scaffolding

--- milestone 1/js/script.js
+++ milestone 1/js/script.js
@@ -1,3 +1,12 @@
+// CONSEGNA
+// Milestone 1
+// [x] Replica della grafica con la possibilità di avere messaggi scritti 
+// dall’utente (verdi) e dall’interlocutore (bianco) assegnando due 
+// classi CSS diverse
+// Visualizzazione dinamica della lista contatti: tramite la direttiva 
+// v-for, visualizzare nome e immagine di ogni contatto
+
+// SVILUPPO
 Vue.config.devtools = true;
 
 const app = new Vue(
